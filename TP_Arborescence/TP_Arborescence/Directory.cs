d6109f6e011807ea8f2ca1376926b2a47ad157dd
Maelle <maelle.blanchet@epsi.fr>
2015-09-18 23:23:58 +0200

    Ajout TP FileSystem

---
+++ TP_Arborescence/TP_Arborescence/Directory.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_Arborescence
+{
+    class Directory:File
+    {
+        //Variables
+        List<File> listeFiles;
+
+        //Constructeur
+        public Directory(string nom, Directory parent): base (nom, parent)
+        {
+            this.Nom = nom;
+            listeFiles = new List<File>();
+            permission = 4;
+        }
+
+        //Méthodes
+        public bool mkdir(string name)
+        {
+            listeFiles.Add(new Directory(name, this));
+            if (listeFiles.Last().Nom == name)
+                return true;
+            else
+                return false;
+        }
+
+        public List<File> ls() 
+        {
+                return this.listeFiles;
+
+        }
+
+        public bool createNewFile(string name)
+        {
+            listeFiles.Add(new File(name, this));
+            if (listeFiles.Last().Nom == name)
+                return true;
+            else
+                return false;
+        }
+
+        public bool delete(string name)
+        {
+            foreach (File file in listeFiles) 
+            {
+                if (file.Nom == name)
+                {
+                    listeFiles.Remove(file);
+                    return true;
+                }
+                else
+                    return false;
+            }
+            return false;
+        }
+
+
+        public File cd(string name)
+        {
+            File courantRemplacer = null;
+            foreach (File file in listeFiles)
+            {
+                if (file.Nom == name)
+                {
+                    courantRemplacer = file;
+                }
+            }
+            return courantRemplacer;
+        }
+
+        public List<File> search(string name)
+        {
+            List<File> retour = null;
+
+
+            retour = new List<File>();
+
+            foreach (File searchDir in listeFiles)
+            {
+
+                if (searchDir.Nom == name)
+                {
+                    retour.Add(searchDir);
+                }
+
+
+                List<File> retour2 = new List<File>();
+                Directory searchDir2 = (Directory)searchDir;
+                retour2 = searchDir2.search(name);
+                foreach (File courant in retour2)
+                {
+                    retour.Add(courant);
+                }
+            }
+            return retour;
+        }
+    }
+}
---
+++ TP_Arborescence/TP_Arborescence/File.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_Arborescence
+{
+    class File
+    {
+        //Variables
+        public string Nom { get; set; }
+        public Directory parent { get; set; }
+        public int permission;
+
+        //Constructeur
+        public File(string nom, Directory parent)
+        {
+            this.Nom = nom;
+            this.parent = parent;
+            permission = 4;
+        }
+
+        //Methodes
+        public bool canWrite()
+        {
+            return (permission & 2) > 0;
+        }
+        public bool canExecute()
+        {
+            return (permission & 1) > 0;
+        }
+        public bool canRead()
+        {
+            return (permission & 4) > 0;
+        }
+        public string getPermission()
+        {
+            string permission = "";
+            if (this.canExecute())
+            {
+                permission = permission + "x";
+            }
+            else if (this.canRead())
+            {
+                permission = permission + "r";
+            }
+            else if (this.canWrite())
+            {
+                permission = permission + "w";
+            }
+            else
+            {
+                permission = permission + "-";
+            }
+            return permission;
+        }
+
+        public string getPath()
+        {
+            File courant = this;
+            string path = "";
+            while (courant.Nom!="/")
+            {
+                path = courant.Nom + "/" + path;
+                courant = courant.parent;
+            }
+            return path;
+
+        }
+            //       Fichier pathFile = this;
+            //string path = "";
+
+            //while (pathFile.Nom != "/")
+            //{
+
+            //    path = pathFile.Nom + "/" + path;
+            //    pathFile = pathFile.Parent;
+
+            //}
+
+
+        public File getRoot()
+        {
+            File courant = this;
+            while (this.parent.Nom !="/" )
+            {
+                courant = this.parent;
+                return courant;
+            }
+            return courant;
+        }
+
+        public bool renameTo(string newName) 
+        {
+            this.Nom = newName;
+            if (Nom == newName)
+                return true;
+            else
+                return false;
+        }
+
+        public File getParent()
+        {
+            File parentRetour = null;
+            if (this.parent != null)
+            {
+                parentRetour = this.parent;
+                return parentRetour;
+            }
+            else
+            {
+                Console.WriteLine("C'est déjà l'origine du monde");
+                return this.parent;
+            }
+        }
+
+        public bool isDirectory()
+        {
+            if (this.GetType() == typeof(Directory))
+                return true;
+            else
+                return false;
+        }
+
+
+        public bool isFile() 
+        {
+            if (this.GetType() == typeof(File))
+                return true;
+            else
+                return false;
+        }
+
+        public string getName() 
+        {
+            string nomFile = this.Nom;
+            return nomFile;            
+        }
+
+        public void chmod(int permission)
+        {
+            this.permission = permission;
+        }
+
+
+    }
+}
